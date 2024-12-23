88a5ef9d2c0fc3cccf57d7e320b0d719150328a9
Bruno Pastre <bruno.pastre@onefootball.com>
2024-12-24 00:23:03 +0100

    sums two integers

--- lib/problem_one.sh
+++ lib/problem_one.sh
@@ -1,3 +1,18 @@
-problem_one() {
-	echo 'Expected syntax: problem_one <list_one> <list_two> where lists are comma separated numbers. Example `problem_one` 1,2,3 4,5,6'
+#!/bin/bash
+
+add() {
+	result=$(( $1 + $2 ))
+	echo $result
 }
+problem_one() {
+	left_col="$1"
+	right_col="$2"
+	if [[ -z $left_col || -z $right_col ]]; then 
+		echo 'Syntax error. Expected problem_one <left_list> <right_list> where lists are comma separated numbers. Example `problem_one 1,2,3 4,5,6`' >&2
+		return 1
+	fi
+	sum=$(add $left_col $right_col)
+	echo  $sum
+}
+
+
--- spec/problem_one_spec.sh
+++ spec/problem_one_spec.sh
@@ -1,12 +1,20 @@
 Describe 'problem_one.sh'
-	Include 'lib/problem_one.sh'
-	It 'fails without list one'
-		When call problem_one
-		The output should equal 'Expected syntax: problem_one <list_one> <list_two> where lists are comma separated numbers. Example `problem_one` 1,2,3 4,5,6'
+	Include lib/problem_one.sh
+	Context 'invalid inputs'
+		It 'fails without list one'
+			When call problem_one
+			The status should be failure
+			The stderr should include 'Syntax error. Expected problem_one <left_list> <right_list> where lists are comma separated numbers. Example `problem_one 1,2,3 4,5,6`'
+		End
+		It 'fails without list two'
+			When call problem_one 1,2,3
+			The status should be failure
+			The error should equal 'Syntax error. Expected problem_one <left_list> <right_list> where lists are comma separated numbers. Example `problem_one 1,2,3 4,5,6`'
+		End
 	End
-	It 'fails without list two'
-		When call problem_one 1,2,3
-		The output should equal 'Expected syntax: problem_one <list_one> <list_two> where lists are comma separated numbers. Example `problem_one` 1,2,3 4,5,6'
+	It 'sums two integers'
+		When call problem_one 2 3
+		The output should equal 5
 	End
 End
 
