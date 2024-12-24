1ab1bcc189e085d39fb6ee45a1a699de8b4c8e41
Bruno Pastre <bruno.pastre@onefootball.com>
2024-12-24 18:25:24 +0100

    problem_two: multiplies and integrates frequencies

--- aoc-2024/01.12/lib/problem_two.sh
+++ aoc-2024/01.12/lib/problem_two.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+TMPBASE=".tmp/problem_two"
+
 count_freq() {
 	echo $2 | grep -o $1 | wc -l | xargs
 }
@@ -5,19 +9,41 @@ count_freq() {
 
 add() {
 	result=$(( $1 + $2 ))
-	echo $result
+	echo "$result"
 }
 
-run() {
-	declare -i sum
+mul() {
+	result=$(( $1 * $2 ))
+	echo "$result"
+}
+
+
+problem_two() {
 	right_list="$(cat $2)"
+	declare -i sum
 	sum=0
 	while read number; do
 		freq=$(count_freq $number "$right_list")
-		sum=$(add $sum $freq)	
+		similarity_score_increase=$(mul $number $freq)
+		sum=$(add $sum $similarity_score_increase)	
 	done <<< "$(cat "$1" | sed 's/ /\n/g')"
 	echo "$sum"
 }
 
+${__SOURCED__:+return}
 
+rm -rf $TMPBASE/**/* || true
+mkdir -p $TMPBASE/../
+mkdir -p $TMPBASE
+
+echo reading
+while read line; do
+	left_number=$(echo $line | sed 's/ .*//g')
+	right_number=$(echo $line | sed 's/.* //g')
+	echo -n "$left_number " >> "$TMPBASE/left_col" 
+	echo -n "$right_number " >> "$TMPBASE/right_col"
+done < input
+echo 'read'
+
+problem_two $TMPBASE/left_col $TMPBASE/right_col
 
--- aoc-2024/01.12/spec/problem_two_spec.sh
+++ aoc-2024/01.12/spec/problem_two_spec.sh
@@ -6,14 +6,18 @@ Describe 'part_two.sh'
 		When call count_freq 3 "3 3 3"
 		The output should equal 3
 	End
-	It 'sums frequencies'
+	It 'multiplies'
+		When call mul 3 3
+		The output should equal 9
+	End
+	It 'sums and multiplies frequencies'
 		mkdir -p $TMPBASE/../
 		mkdir -p $TMPBASE
 		mkdir -p "$TMPBASE/test"
 		echo '3 2 3' > $TMPBASE/test/sums_freq_left
 		echo '3 3 3' > $TMPBASE/test/sums_freq_right
-		When call run $TMPBASE/test/sums_freq_left $TMPBASE/test/sums_freq_right
-		The output should equal 6
+		When call problem_two $TMPBASE/test/sums_freq_left $TMPBASE/test/sums_freq_right
+		The output should equal 18
 	End
 End
 
